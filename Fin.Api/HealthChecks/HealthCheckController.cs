46b736a8203c7a472f3b94617133294a7a2e3cec
RafaelKC <rafaelkaua97@gmail.com>
2025-05-02 23:36:45 -0300

    FIN-25 added health check endpoint

---
+++ Fin.Api/HealthChecks/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Fin.Application.HealthChecks.Dtos;
+using Fin.Application.HealthChecks.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fin.Api.HealthChecks;
+
+[Route("health")]
+public class HealthCheckController(IHealthCheckService healthCheckService) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<HealthCheckOutput> Get()
+    {
+        return healthCheckService.GetHealthCheck();
+    }
+}
---
+++ Fin.Application/HealthChecks/Dtos/HealthCheckOutput.cs
@@ -0,0 +1,8 @@
+﻿namespace Fin.Application.HealthChecks.Dtos;
+
+public class HealthCheckOutput
+{
+    public string Status { get; set; }
+    public string Version { get; set; }
+    public DateTime Timestamp { get; set; }
+}
