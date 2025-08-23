54fdb081bd0a3494bfa66f2822ec3318eb667da0
RafaelKC <rafaelkaua97@gmail.com>
2025-08-23 02:52:52 -0300

    FIN-22 adjusting cors

--- Fin.Api/Program.cs
+++ Fin.Api/Program.cs
@@ -9,6 +9,16 @@ builder.Services
     .AddInfrastructure(builder.Configuration)
     .AddNotifications()
     .AddOpenApiDocument()
+    .AddCors(options =>
+    {
+        options.AddPolicy("AllowAngularLocalhost",
+            policy =>
+            {
+                policy.WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+    })
     .AddControllers();
 
 builder.Logging.SetMinimumLevel(LogLevel.Debug);
@@ -20,6 +30,7 @@ if (app.Environment.IsDevelopment())
     app.UseOpenApi();
     app.UseSwaggerUi();
     app.UseHangfireDashboard();
+    app.UseCors("AllowAngularLocalhost");
 }
 
 
