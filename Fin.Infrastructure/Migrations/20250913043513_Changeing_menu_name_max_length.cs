7ca4bb9e21fe623823bc801bee97cc8af7b6c4b2
¨Rafael <rafaelkaua97@gmail.com>
2025-09-13 01:55:53 -0300

    FIN-23 changed menu name max length 20 -> 100

---
+++ Fin.Infrastructure/Migrations/20250913043513_Changeing_menu_name_max_length.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fin.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changeing_menu_name_max_length : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "public",
+                table: "Menus",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "public",
+                table: "Menus",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
