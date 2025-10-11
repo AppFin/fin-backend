ac87aa3fcac56b392b9992abaac49baf914a3885
¨Rafael <rafaelkaua97@gmail.com>
2025-10-11 13:13:25 -0300

    FIN-34 adjusted

--- Fin.Api/TitleCategories/TitleCategoryController.cs
+++ Fin.Api/TitleCategories/TitleCategoryController.cs
@@ -1,61 +1,61 @@
-﻿    using Fin.Application.TitleCategories;
-    using Fin.Application.TitleCategories.Dtos;
-    using Fin.Application.TitleCategories.Enums;
-    using Fin.Domain.Global.Classes;
-    using Fin.Domain.TitleCategories.Dtos;
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Mvc;
+﻿using Fin.Application.TitleCategories;
+using Fin.Application.TitleCategories.Dtos;
+using Fin.Application.TitleCategories.Enums;
+using Fin.Domain.Global.Classes;
+using Fin.Domain.TitleCategories.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-    namespace Fin.Api.TitleCategories;
+namespace Fin.Api.TitleCategories;
 
-    [Route("title-categories")]
-    [Authorize]
-    public class TitleCategoryController(ITitleCategoryService service) : ControllerBase
+[Route("title-categories")]
+[Authorize]
+public class TitleCategoryController(ITitleCategoryService service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<PagedOutput<TitleCategoryOutput>> GetList([FromQuery] TitleCategoryGetListInput input)
     {
-        [HttpGet]
-        public async Task<PagedOutput<TitleCategoryOutput>> GetList([FromQuery] TitleCategoryGetListInput input)
-        {
-            return await service.GetList(input);
-        }
-
-        [HttpGet("{id:guid}")]
-        public async Task<ActionResult<TitleCategoryOutput>> Get([FromRoute] Guid id)
-        {
-            var category = await service.Get(id);
-            return category != null ? Ok(category) : NotFound();
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<TitleCategoryOutput>> Create([FromBody] TitleCategoryInput input)
-        {
-            var validationResult = await service.Create(input, autoSave: true);
-            return validationResult.Success
-                ? Created($"categories/{validationResult.Data?.Id}", validationResult.Data)
-                : UnprocessableEntity(validationResult);
-        }
-
-        [HttpPut("{id:guid}")]
-        public async Task<ActionResult> Update([FromRoute] Guid id, [FromBody] TitleCategoryInput input)
-        {
-            var validationResult = await service.Update(id, input, autoSave: true);
-            return validationResult.Success
-                ? Ok()
-                : validationResult.ErrorCode == TitleCategoryCreateOrUpdateErrorCode.TitleCategoryNotFound
-                    ? NotFound(validationResult)
-                    : UnprocessableEntity(validationResult);
-        }
-
-        [HttpPut("toggle-inactivated/{id:guid}")]
-        public async Task<ActionResult> ToggleInactivated([FromRoute] Guid id)
-        {
-            var updated = await service.ToggleInactive(id, autoSave: true);
-            return updated ? Ok() : NotFound();
-        }
-
-        [HttpDelete("{id:guid}")]
-        public async Task<ActionResult> Delete([FromRoute] Guid id)
-        {
-            var deleted = await service.Delete(id, autoSave: true);
-            return deleted ? Ok() : NotFound();
-        }
+        return await service.GetList(input);
     }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<TitleCategoryOutput>> Get([FromRoute] Guid id)
+    {
+        var category = await service.Get(id);
+        return category != null ? Ok(category) : NotFound();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<TitleCategoryOutput>> Create([FromBody] TitleCategoryInput input)
+    {
+        var validationResult = await service.Create(input, autoSave: true);
+        return validationResult.Success
+            ? Created($"categories/{validationResult.Data?.Id}", validationResult.Data)
+            : UnprocessableEntity(validationResult);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> Update([FromRoute] Guid id, [FromBody] TitleCategoryInput input)
+    {
+        var validationResult = await service.Update(id, input, autoSave: true);
+        return validationResult.Success
+            ? Ok()
+            : validationResult.ErrorCode == TitleCategoryCreateOrUpdateErrorCode.TitleCategoryNotFound
+                ? NotFound(validationResult)
+                : UnprocessableEntity(validationResult);
+    }
+
+    [HttpPut("toggle-inactivated/{id:guid}")]
+    public async Task<ActionResult> ToggleInactivated([FromRoute] Guid id)
+    {
+        var updated = await service.ToggleInactive(id, autoSave: true);
+        return updated ? Ok() : NotFound();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> Delete([FromRoute] Guid id)
+    {
+        var deleted = await service.Delete(id, autoSave: true);
+        return deleted ? Ok() : NotFound();
+    }
+}
