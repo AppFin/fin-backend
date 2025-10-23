8d94655da30d7a98a33b1f0e0e5b9077bc8a1c73
¨Rafael <rafaelkaua97@gmail.com>
2025-10-23 20:57:08 -0300

    FIN-43 adding title to wallet validation

--- Fin.Application/Wallets/Enums/WalletDeleteErrorCode.cs
+++ Fin.Application/Wallets/Enums/WalletDeleteErrorCode.cs
@@ -1,9 +1,18 @@
+using Fin.Infrastructure.Errors;
+
 namespace Fin.Application.Wallets.Enums;
 
 public enum WalletDeleteErrorCode
 {
+    [ErrorMessage("Wallet not found")]
     WalletNotFound = 0,
+    
+    [ErrorMessage("Wallet in use by titles")]
     WalletInUseByTitles = 1,
+    
+    [ErrorMessage("Wallet in use by credit cards")]
     WalletInUseByCreditCards = 2,
+    
+    [ErrorMessage("Wallet in use by credit card and titles")]
     WalletInUseByCreditCardsAndTitle = 3,
 }
